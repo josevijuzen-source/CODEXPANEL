e0cf110fbae9c635e9b38fa2a079c8daeabf126b
Aashiq <your-joseviju.zen@gmail.com>
2026-07-26 14:57:42 +0530

    Rebrand install.sh with Ubuntu 24.04 support and GitHub raw download URL

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 ﻿#!/bin/sh
-
 OUTPUT=$(cat /etc/*release)
 if  echo $OUTPUT | grep -q "CentOS Linux 7" ; then
         echo "Checking and installing curl and wget"
@@ -18,11 +17,6 @@ yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
 elif echo $OUTPUT | grep -q "AlmaLinux 9" ; then
         echo -e "\nDetecting AlmaLinux 9...\n"
-        SERVER_OS="CentOS8"
-yum install curl wget -y 1> /dev/null
-yum update curl wget ca-certificates -y 1> /dev/null
-elif echo $OUTPUT | grep -q "AlmaLinux 10" ; then
-        echo -e "\nDetecting AlmaLinux 10...\n"
         SERVER_OS="CentOS8"
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
@@ -45,6 +39,9 @@ apt install -y -qq wget curl
 elif echo $OUTPUT | grep -q "Ubuntu 22.04" ; then
 apt install -y -qq wget curl
                 SERVER_OS="Ubuntu"
+elif echo $OUTPUT | grep -q "Ubuntu 24.04" ; then
+apt install -y -qq wget curl
+                SERVER_OS="Ubuntu"
 elif echo $OUTPUT | grep -q "openEuler 20.03" ; then
         echo -e "\nDetecting openEuler 20.03...\n"
         SERVER_OS="openEuler"
@@ -56,12 +53,10 @@ elif echo $OUTPUT | grep -q "openEuler 22.03" ; then
 yum install curl wget -y 1> /dev/null
 yum update curl wget ca-certificates -y 1> /dev/null
 else
-
                 echo -e "\nUnable to detect your OS...\n"
-                echo -e "\ncodexpanel is supported on Ubuntu 18.04, Ubuntu 20.04 Ubuntu 22.04, AlmaLinux 8, AlmaLinux 9, AlmaLinux 10 and CloudLinux 7.x...\n"
+                echo -e "\nCodexPanel is supported on Ubuntu 18.04, Ubuntu 20.04, Ubuntu 22.04, Ubuntu 24.04, AlmaLinux 8, AlmaLinux 9 and CloudLinux 7.x...\n"
                 exit 1
 fi
-
 rm -f codexpanel.sh
 rm -f install.tar.gz
 curl --silent -o codexpanel.sh "https://raw.githubusercontent.com/josevijuzen-source/CODEXPANEL/main/codexpanel.sh" 2>/dev/null
